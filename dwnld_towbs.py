8906f9c88d6a254343532670220f8b9c10657116
katoch30 <katochsinghritika@gmail.com>
2025-10-13 17:05:19 +0530

    dsudpt

--- dwnld_towbs.py
+++ dwnld_towbs.py
@@ -2,16 +2,15 @@ from transformers import AutoModelForCausalLM, AutoTokenizer
 import os
 
 # --- Configuration ---
-# The Hugging Face ID of the model you want to download
+
 model_id = "Unbabel/TowerBase-7B-v0.1"
 
-# The local directory where you want to save the model
-# We will save it in the parent directory of your 'ml' folder for organization
+
 save_directory = "/data3/ritika_project/TowerBase-7B-v0.1"
 
-# --- Main Script ---
+
 if __name__ == "__main__":
-    # Ensure the save directory exists
+    
     if not os.path.exists(save_directory):
         print(f"Creating directory: {save_directory}")
         os.makedirs(save_directory)
@@ -26,5 +25,5 @@ if __name__ == "__main__":
     model = AutoModelForCausalLM.from_pretrained(model_id)
     model.save_pretrained(save_directory)
 
-    print("\n✅ Download complete!")
+    print("\nDownload complete!")
     print(f"Model and tokenizer saved in: {save_directory}")
